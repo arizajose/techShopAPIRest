59979de83673ef26b87809417a6c4a0f3ce4818d
arizajose <arizasalcedoja@gmail.com>
2022-07-02 21:41:31 -0500

    JAS - Registrando las URIs de los servicios rest para la gestión de cada tabla de la base de datos.

--- routes/web.php
+++ routes/web.php
@@ -35,3 +35,60 @@ $router->delete('/libros/{idLibro}', 'LibroController@borrar'); //usando un path
 
 //Rest Service para actualizar un registro de libro especifico
 $router->put('/libros/actualizar/{idLibro}', 'LibroController@actualizar');  //usando un pathparam para enviar el id
+
+//URIs para Categorias
+$router->get('/categorias', 'CategoriaController@getCategorias');
+$router->get('/categorias/{id}', 'CategoriaController@getCategoriaById');
+$router->post('/categorias', 'CategoriaController@guardarCategoria');
+
+//URIs para Productos
+$router->get('/productos', 'ProductoController@getProductos');
+$router->get('/productos/{id}', 'ProductoController@getProductoById');
+$router->post('/productos', 'ProductoController@guardarProducto');
+$router->delete('/productos/{id}', 'ProductoController@deleteProductoById');
+$router->put('/productos/{id}', 'ProductoController@updateProductoById');
+
+//URIs para PerfilSystem
+$router->get('/perfilsystem', 'PerfilSystemController@getPerfilSystem');
+$router->get('/perfilsystem/{id}', 'PerfilSystemController@getPerfilSystemById');
+$router->post('/perfilsystem', 'PerfilSystemController@guardarPerfilSystem');
+
+//URIs para Clientes
+$router->get('/clientes', 'ClienteController@getClientes');
+$router->get('/clientes/{id}', 'ClienteController@getClienteById');
+$router->post('/clientes', 'ClienteController@guardarCliente');
+
+//URIs para Empleados
+$router->get('/empleados', 'EmpleadoController@getEmpleados');
+$router->get('/empleados/{id}', 'EmpleadoController@getEmpleadoById');
+$router->post('/empleados', 'EmpleadoController@guardarEmpleado');
+
+//URIs para Comprobantes
+$router->get('/comprobantes', 'ComprobanteController@getComprobantes');
+$router->get('/comprobantes/{id}', 'ComprobanteController@getComprobanteById');
+$router->post('/comprobantes', 'ComprobanteController@guardarComprobante');
+
+//URIs para Ventas
+$router->get('/ventas', 'VentaController@getVentas');
+$router->get('/ventas/{id}', 'VentaController@getVentaById');
+$router->post('/ventas', 'VentaController@guardarVenta');
+$router->delete('/ventas/{id}', 'VentaController@deleteVentaById');
+$router->put('/ventas/{id}', 'VentaController@updateVentaById');
+
+//URIs para DetalleVentas
+$router->get('/detalleventas/{id}', 'DetalleVentaController@getDetalleVentaByVentaId');
+$router->post('/detalleventas', 'DetalleVentaController@guardarDetalleVenta');
+
+//URIs para Boletas
+$router->get('/boletas', 'BoletaController@getBoletas');
+$router->get('/boletas/{id}', 'BoletaController@getBoletaById');
+$router->post('/boletas', 'BoletaController@guardarBoleta');
+$router->delete('/boletas/{id}', 'BoletaController@deleteBoletaById');
+$router->put('/boletas/{id}', 'BoletaController@updateBoletaById');
+
+//URIs para Facturas
+$router->get('/facturas', 'FacturaController@getFacturas');
+$router->get('/facturas/{id}', 'FacturaController@getFacturaById');
+$router->post('/facturas', 'FacturaController@guardarFactura');
+$router->delete('/facturas/{id}', 'FacturaController@deleteFacturaById');
+$router->put('/facturas/{id}', 'FacturaController@updateFacturaById');
